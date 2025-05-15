e247c97b450057f8c606c6278cffff40c7febf10
Thomas Rigby <thom@scrummable.com>
2025-05-15 16:21:29 +0100

    surfaces nuance

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = function (eleventyConfig) {
   // Passthrough copy for CSS files
   eleventyConfig.addPassthroughCopy("src/css");
 
-  eleventyConfig.addFilter("winner", ({ iphone, android }) => iphone > android ? "apple" : "android");
+  eleventyConfig.addFilter("winner", ({ iphone, android }) => iphone > android ? 223 : 147);
+    eleventyConfig.addFilter("weighting", ({ iphone, android }) => iphone > android ? `${Math.ceil(iphone / 10) * 10}` : `${Math.ceil(android / 10) * 10}`);
 
   return {
     markdownTemplateEngine: 'njk',
